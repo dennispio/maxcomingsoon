103c9a6a610ccf995ecd7ebad4cc62f57ede0eda
dennispio <dennis.piotrowski@web.de>
2019-02-02 12:20:02 +0100

    impressum / datenschutz

--- src/components/bottommorph/bottommorph.jsx
+++ src/components/bottommorph/bottommorph.jsx
@@ -6,8 +6,20 @@ import bottommorphpic from '../../img/bubble_left.png'
 const Bottommorph = () =>{
     return(
         <div className="bottommorph-container">
-        <img  alt="bottommorph" src={bottommorphpic}></img>
-        </div>
+          <img  alt="bottommorph" src={bottommorphpic}></img>
+          <div className="link-container"> 
+                <div className="link">
+                    <a href="">
+                        Datenschutz
+                    </a>
+                </div>
+                <div className="link">
+                    <a href="">
+                        Impressum
+                    </a>
+                </div>                   
+            </div>
+          </div>
     )
 }
 
